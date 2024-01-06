f54796556608d6124c984001042d11fde892f24b
kadu-v <kazuonsunday@yahoo.co.jp>
2024-01-06 04:21:38 +0000

    Add: check signed extention

--- test_bench/tb_riscv_extend.cpp
+++ test_bench/tb_riscv_extend.cpp
@@ -50,6 +50,16 @@ int main(int argc, char** argv) {
   assert_eq("[S type] positive value check", dut->imm_s_sext, expected);
 
   /* J type */
+  // dut->inst = 0b1_0101010101_1_11010101_000000000000;
+  dut->inst = 0b10101010101111010101000000000000;
+  dut->eval();
+  expected = 0b11111111111111010101101010101010;
+  assert_eq("[J type] negative value check", dut->imm_j_sext, expected);
 
   /* B type*/
+  // dut->inst = 0b1_010101_00000_00000_000_1010_1_0000000;
+  dut->inst = 0b10101010000000000000101010000000;
+  dut->eval();
+  expected = 0b111111111111111111111101010110100;
+  assert_eq("[B type] negative value check", dut->imm_b_sext, expected);
 }
