a2d0e298b6d2d16dff63801e65fdafd398c183f1
kadu-v <kazuonsunday@yahoo.co.jp>
2024-08-25 10:06:31 +0000

    Revert: All tests is passed (i.e., riscv-tests and unit tests)

--- test_bench/riscv_defs.h
+++ test_bench/riscv_defs.h
@@ -7,7 +7,8 @@
 /* --------------------------------------------------------------------- *
     Utils type
  * ---------------------------------------------------------------------*/
-void printBinaryWithPadding32(int number) {
+void printBinaryWithPadding32(int number)
+{
   std::bitset<32> binaryRepresentation(number);
   std::cout << binaryRepresentation << std::endl;
 }
@@ -18,7 +19,8 @@ void printBinaryWithPadding32(int number) {
 #define INST_R_MASK 0b11111110000000000111000001111111
 
 uint32_t r_inst(uint32_t funct7, uint32_t rs2, uint32_t rs1, uint32_t funct3,
-                uint32_t rd, uint32_t opcode) {
+                uint32_t rd, uint32_t opcode)
+{
   uint32_t inst = funct7 << 25;
   inst += (rs2 << 20);
   inst += (rs1 << 15);
@@ -38,7 +40,8 @@ uint32_t r_inst(uint32_t funct7, uint32_t rs2, uint32_t rs1, uint32_t funct3,
 
 // add rd, rs1, rs2
 // x[rd] = x[rs1] + x[rs2]
-uint32_t add(uint32_t rd, uint32_t rs1, uint32_t rs2) {
+uint32_t add(uint32_t rd, uint32_t rs1, uint32_t rs2)
+{
   return r_inst(FUNCT7_ADD, rs2, rs1, FUNCT3_ADD, rd, OPCODE_ADD);
 }
 
@@ -52,7 +55,8 @@ uint32_t add(uint32_t rd, uint32_t rs1, uint32_t rs2) {
 
 // sub rd, rs1, rs2
 // x[rd] = x[rs1] - x[rs2]
-uint32_t sub(uint32_t rd, uint32_t rs1, uint32_t rs2) {
+uint32_t sub(uint32_t rd, uint32_t rs1, uint32_t rs2)
+{
   return r_inst(FUNCT7_SUB, rs2, rs1, FUNCT3_SUB, rd, OPCODE_SUB);
 }
 
@@ -66,7 +70,8 @@ uint32_t sub(uint32_t rd, uint32_t rs1, uint32_t rs2) {
 
 // sll rd, rs1, rs2
 // x[rd] = x[rs1] << x[rs2]
-uint32_t sll(uint32_t rd, uint32_t rs1, uint32_t rs2) {
+uint32_t sll(uint32_t rd, uint32_t rs1, uint32_t rs2)
+{
   return r_inst(FUNCT7_SLL, rs2, rs1, FUNCT3_SLL, rd, OPCODE_SLL);
 }
 
@@ -80,7 +85,8 @@ uint32_t sll(uint32_t rd, uint32_t rs1, uint32_t rs2) {
 
 // slt rd, rs1, rs2
 // x[rd] = x[rs1] <s x[rs2]
-uint32_t slt(uint32_t rd, uint32_t rs1, uint32_t rs2) {
+uint32_t slt(uint32_t rd, uint32_t rs1, uint32_t rs2)
+{
   return r_inst(FUNCT7_SLT, rs2, rs1, FUNCT3_SLT, rd, OPCODE_SLT);
 }
 
@@ -94,7 +100,8 @@ uint32_t slt(uint32_t rd, uint32_t rs1, uint32_t rs2) {
 
 // sltu rd, rs1, rs2
 // x[rd] = x[rs1] <u x[rs2]
-uint32_t sltu(uint32_t rd, uint32_t rs1, uint32_t rs2) {
+uint32_t sltu(uint32_t rd, uint32_t rs1, uint32_t rs2)
+{
   return r_inst(FUNCT7_SLTU, rs2, rs1, FUNCT3_SLTU, rd, OPCODE_SLTU);
 }
 
@@ -108,7 +115,8 @@ uint32_t sltu(uint32_t rd, uint32_t rs1, uint32_t rs2) {
 
 // xor rd, rs1, rs2
 // x[rd] = x[rs1] ^ x[rs2]
-uint32_t ixor(uint32_t rd, uint32_t rs1, uint32_t rs2) {
+uint32_t ixor(uint32_t rd, uint32_t rs1, uint32_t rs2)
+{
   return r_inst(FUNCT7_XOR, rs2, rs1, FUNCT3_XOR, rd, OPCODE_XOR);
 }
 
@@ -122,7 +130,8 @@ uint32_t ixor(uint32_t rd, uint32_t rs1, uint32_t rs2) {
 
 // srl rd, rs1, rs2
 // x[rd] = x[rs1] >>u x[rs2]
-uint32_t srl(uint32_t rd, uint32_t rs1, uint32_t rs2) {
+uint32_t srl(uint32_t rd, uint32_t rs1, uint32_t rs2)
+{
   return r_inst(FUNCT7_SRL, rs2, rs1, FUNCT3_SRL, rd, OPCODE_SRL);
 }
 
@@ -136,7 +145,8 @@ uint32_t srl(uint32_t rd, uint32_t rs1, uint32_t rs2) {
 
 // sra rd, rs1, rs2
 // x[rd] = x[rs1] >>s x[rs2]
-uint32_t sra(uint32_t rd, uint32_t rs1, uint32_t rs2) {
+uint32_t sra(uint32_t rd, uint32_t rs1, uint32_t rs2)
+{
   return r_inst(FUNCT7_SRA, rs2, rs1, FUNCT3_SRA, rd, OPCODE_SRA);
 }
 
@@ -150,7 +160,8 @@ uint32_t sra(uint32_t rd, uint32_t rs1, uint32_t rs2) {
 
 // or rd, rs1, rs2
 // x[rd] = x[rs1] | x[rs2]
-uint32_t ior(uint32_t rd, uint32_t rs1, uint32_t rs2) {
+uint32_t ior(uint32_t rd, uint32_t rs1, uint32_t rs2)
+{
   return r_inst(FUNCT7_OR, rs2, rs1, FUNCT3_OR, rd, OPCODE_OR);
 }
 
@@ -164,7 +175,8 @@ uint32_t ior(uint32_t rd, uint32_t rs1, uint32_t rs2) {
 
 // and rd, rs1, rs2
 // x[rd] = x[rs1] & x[rs2]
-uint32_t iand(uint32_t rd, uint32_t rs1, uint32_t rs2) {
+uint32_t iand(uint32_t rd, uint32_t rs1, uint32_t rs2)
+{
   return r_inst(FUNCT7_AND, rs2, rs1, FUNCT3_AND, rd, OPCODE_AND);
 }
 
@@ -174,7 +186,8 @@ uint32_t iand(uint32_t rd, uint32_t rs1, uint32_t rs2) {
 #define INST_I_MASK 0b0000000000000000111000001111111
 
 uint32_t i_inst(uint32_t imm, uint32_t rs1, uint32_t funct3, uint32_t rd,
-                uint32_t opcode) {
+                uint32_t opcode)
+{
   uint32_t inst = imm << 20;
   inst += (rs1 << 15);
   inst += (funct3 << 12);
@@ -192,7 +205,8 @@ uint32_t i_inst(uint32_t imm, uint32_t rs1, uint32_t funct3, uint32_t rd,
 
 // jalr rd, rs1, imm
 // t = pc+4; pc = (x[rs1] + sext(imm)) & ~1; x[rd] = t
-uint32_t jalr(uint32_t rd, uint32_t rs1, uint32_t imm) {
+uint32_t jalr(uint32_t rd, uint32_t rs1, uint32_t imm)
+{
   return i_inst(imm, rs1, FUNCT3_JALR, rd, OPCODE_JALR);
 }
 
@@ -205,7 +219,8 @@ uint32_t jalr(uint32_t rd, uint32_t rs1, uint32_t imm) {
 
 // addi rd, rs1, imm
 // x[rd] = x[rs1] + sext(imm)
-uint32_t addi(uint32_t rd, uint32_t rs1, uint32_t imm) {
+uint32_t addi(uint32_t rd, uint32_t rs1, uint32_t imm)
+{
   return i_inst(imm, rs1, FUNCT3_ADDI, rd, OPCODE_ADDI);
 }
 
@@ -218,7 +233,8 @@ uint32_t addi(uint32_t rd, uint32_t rs1, uint32_t imm) {
 
 // slti rd, rs1, imm
 // x[rd] = x[rs1] <s sext(imm)
-uint32_t slti(uint32_t rd, uint32_t rs1, uint32_t imm) {
+uint32_t slti(uint32_t rd, uint32_t rs1, uint32_t imm)
+{
   return i_inst(imm, rs1, FUNCT3_SLTI, rd, OPCODE_SLTI);
 }
 
@@ -231,7 +247,8 @@ uint32_t slti(uint32_t rd, uint32_t rs1, uint32_t imm) {
 
 // sltiu rd, rs1, imm
 // x[rd] = x[rs1] <u sext(imm)
-uint32_t sltiu(uint32_t rd, uint32_t rs1, uint32_t imm) {
+uint32_t sltiu(uint32_t rd, uint32_t rs1, uint32_t imm)
+{
   return i_inst(imm, rs1, FUNCT3_SLTIU, rd, OPCODE_SLTIU);
 }
 
@@ -244,7 +261,8 @@ uint32_t sltiu(uint32_t rd, uint32_t rs1, uint32_t imm) {
 
 // xori rd, rs1, imm
 // x[rd] = x[rs1] ^ sext(imm)
-uint32_t xori(uint32_t rd, uint32_t rs1, uint32_t imm) {
+uint32_t xori(uint32_t rd, uint32_t rs1, uint32_t imm)
+{
   return i_inst(imm, rs1, FUNCT3_XORI, rd, OPCODE_XORI);
 }
 
@@ -257,7 +275,8 @@ uint32_t xori(uint32_t rd, uint32_t rs1, uint32_t imm) {
 
 // ori rd, rs1, imm
 // x[rd] = x[rs1] | sext(imm)
-uint32_t ori(uint32_t rd, uint32_t rs1, uint32_t imm) {
+uint32_t ori(uint32_t rd, uint32_t rs1, uint32_t imm)
+{
   return i_inst(imm, rs1, FUNCT3_ORI, rd, OPCODE_ORI);
 }
 
@@ -270,7 +289,8 @@ uint32_t ori(uint32_t rd, uint32_t rs1, uint32_t imm) {
 
 // andi rd, rs1, imm
 // x[rd] = x[rs1] & sext(imm)
-uint32_t andi(uint32_t rd, uint32_t rs1, uint32_t imm) {
+uint32_t andi(uint32_t rd, uint32_t rs1, uint32_t imm)
+{
   return i_inst(imm, rs1, FUNCT3_ANDI, rd, OPCODE_ANDI);
 }
 
@@ -283,7 +303,8 @@ uint32_t andi(uint32_t rd, uint32_t rs1, uint32_t imm) {
 
 // slli rd, rs1, imm
 // x[rd] = x[rs1] << imm[4:0]
-uint32_t slli(uint32_t rd, uint32_t rs1, uint32_t imm) {
+uint32_t slli(uint32_t rd, uint32_t rs1, uint32_t imm)
+{
   return i_inst(imm, rs1, FUNCT3_SLLI, rd, OPCODE_SLLI);
 }
 
@@ -296,7 +317,8 @@ uint32_t slli(uint32_t rd, uint32_t rs1, uint32_t imm) {
 
 // srli rd, rs1, imm
 // x[rd] = x[rs1] >>u imm[4:0]
-uint32_t srli(uint32_t rd, uint32_t rs1, uint32_t imm) {
+uint32_t srli(uint32_t rd, uint32_t rs1, uint32_t imm)
+{
   return i_inst(imm, rs1, FUNCT3_SRLI, rd, OPCODE_SRLI);
 }
 
@@ -309,7 +331,8 @@ uint32_t srli(uint32_t rd, uint32_t rs1, uint32_t imm) {
 
 // srai rd, rs1, imm
 // x[rd] = x[rs1] >>s imm[4:0]
-uint32_t srai(uint32_t rd, uint32_t rs1, uint32_t imm) {
+uint32_t srai(uint32_t rd, uint32_t rs1, uint32_t imm)
+{
   uint32_t imm_i = 0b10000000000 + imm;
   return i_inst(imm_i, rs1, FUNCT3_SRAI, rd, OPCODE_SRAI);
 }
@@ -323,7 +346,8 @@ uint32_t srai(uint32_t rd, uint32_t rs1, uint32_t imm) {
 
 // lb rd, rs1, imm
 // x[rd] = sext(M[ x[rs1] + sext(imm) ][7:0])
-uint32_t lb(uint32_t rd, uint32_t rs1, uint32_t imm_i) {
+uint32_t lb(uint32_t rd, uint32_t rs1, uint32_t imm_i)
+{
   return i_inst(imm_i, rs1, FUNCT3_LB, rd, OPCODE_LB);
 }
 
@@ -336,7 +360,8 @@ uint32_t lb(uint32_t rd, uint32_t rs1, uint32_t imm_i) {
 
 // lh rd, rs1, imm
 // x[rd] = sext(M[ x[rs1] + sext(imm) ][15:0])
-uint32_t lh(uint32_t rd, uint32_t rs1, uint32_t imm_i) {
+uint32_t lh(uint32_t rd, uint32_t rs1, uint32_t imm_i)
+{
   return i_inst(imm_i, rs1, FUNCT3_LH, rd, OPCODE_LH);
 }
 
@@ -349,7 +374,8 @@ uint32_t lh(uint32_t rd, uint32_t rs1, uint32_t imm_i) {
 
 // lw rd, rs1, imm
 // x[rd] = sext(M[ x[rs1] + sext(imm) ])
-uint32_t lw(uint32_t rd, uint32_t rs1, uint32_t imm_i) {
+uint32_t lw(uint32_t rd, uint32_t rs1, uint32_t imm_i)
+{
   return i_inst(imm_i, rs1, FUNCT3_LW, rd, OPCODE_LW);
 }
 
@@ -362,7 +388,8 @@ uint32_t lw(uint32_t rd, uint32_t rs1, uint32_t imm_i) {
 
 // lbu rd, rs1, imm
 // x[rd] = M[ x[rs1] + sext(imm) ][7:0]
-uint32_t lbu(uint32_t rd, uint32_t rs1, uint32_t imm_i) {
+uint32_t lbu(uint32_t rd, uint32_t rs1, uint32_t imm_i)
+{
   return i_inst(imm_i, rs1, FUNCT3_LBU, rd, OPCODE_LBU);
 }
 
@@ -375,7 +402,8 @@ uint32_t lbu(uint32_t rd, uint32_t rs1, uint32_t imm_i) {
 
 // lhu rd, rs1, imm
 // x[rd] = M[ x[rs1] + sext(imm) ][15:0]
-uint32_t lhu(uint32_t rd, uint32_t rs1, uint32_t imm_i) {
+uint32_t lhu(uint32_t rd, uint32_t rs1, uint32_t imm_i)
+{
   return i_inst(imm_i, rs1, FUNCT3_LHU, rd, OPCODE_LHU);
 }
 
@@ -388,7 +416,8 @@ uint32_t lhu(uint32_t rd, uint32_t rs1, uint32_t imm_i) {
 
 // csrrw rd, csr, rs1
 // t = CSRs[csr]; CSRs[csr] = x[rs1]; x[rd] = t
-uint32_t csrrw(uint32_t rd, uint32_t csr, uint32_t rs1) {
+uint32_t csrrw(uint32_t rd, uint32_t csr, uint32_t rs1)
+{
   return i_inst(csr, rs1, FUNCT3_CSRRW, rd, OPCODE_CSRRW);
 }
 
@@ -401,7 +430,8 @@ uint32_t csrrw(uint32_t rd, uint32_t csr, uint32_t rs1) {
 
 // csrrwi rd, csr, zimm[4:0]
 // x[rd] = CSRs[csr]; CSRs[csr] = zimm
-uint32_t csrrwi(uint32_t rd, uint32_t csr, uint32_t zimm) {
+uint32_t csrrwi(uint32_t rd, uint32_t csr, uint32_t zimm)
+{
   return i_inst(csr, zimm, FUNCT3_CSRRWI, rd, OPCODE_CSRRWI);
 }
 
@@ -414,7 +444,8 @@ uint32_t csrrwi(uint32_t rd, uint32_t csr, uint32_t zimm) {
 
 // csrrs rd, csr, rs1
 // t = CSRs[csr]; CSRs[csr] = t | x[rs1]; x[rd] = t
-uint32_t csrrs(uint32_t rd, uint32_t csr, uint32_t rs1) {
+uint32_t csrrs(uint32_t rd, uint32_t csr, uint32_t rs1)
+{
   return i_inst(csr, rs1, FUNCT3_CSRRS, rd, OPCODE_CSRRS);
 }
 
@@ -427,7 +458,8 @@ uint32_t csrrs(uint32_t rd, uint32_t csr, uint32_t rs1) {
 
 // csrrsi rd, csr, rs1
 // t = CSRs[csr]; CSRs[csr] = t | zimm; x[rd] = t
-uint32_t csrrsi(uint32_t rd, uint32_t csr, uint32_t zimm) {
+uint32_t csrrsi(uint32_t rd, uint32_t csr, uint32_t zimm)
+{
   return i_inst(csr, zimm, FUNCT3_CSRRSI, rd, OPCODE_CSRRSI);
 }
 
@@ -440,7 +472,8 @@ uint32_t csrrsi(uint32_t rd, uint32_t csr, uint32_t zimm) {
 
 // csrrc rd, csr, rs1
 // t = CSRs[csr]; CSRs[csr] = t & ~x[rs1]; x[rd] = t
-uint32_t csrrc(uint32_t rd, uint32_t csr, uint32_t rs1) {
+uint32_t csrrc(uint32_t rd, uint32_t csr, uint32_t rs1)
+{
   return i_inst(csr, rs1, FUNCT3_CSRRC, rd, OPCODE_CSRRC);
 }
 
@@ -453,7 +486,8 @@ uint32_t csrrc(uint32_t rd, uint32_t csr, uint32_t rs1) {
 
 // csrrci rd, csr, rs1
 // t = CSRs[csr]; CSRs[csr] = t & ~zimm; x[rd] = t
-uint32_t csrrci(uint32_t rd, uint32_t csr, uint32_t zimm) {
+uint32_t csrrci(uint32_t rd, uint32_t csr, uint32_t zimm)
+{
   return i_inst(csr, zimm, FUNCT3_CSRRCI, rd, OPCODE_CSRRCI);
 }
 
@@ -471,7 +505,8 @@ uint32_t ecall() { return INST_ECALL; }
 #define INST_S_MASK 0b00000000000000000111000001111111
 
 uint32_t s_inst(uint32_t imm_s2, uint32_t rs2, uint32_t rs1, uint32_t funct3,
-                uint32_t imm_s1, uint32_t opcode) {
+                uint32_t imm_s1, uint32_t opcode)
+{
   uint32_t inst = (imm_s2 << 25);
   inst += (rs2 << 20);
   inst += (rs1 << 15);
@@ -490,7 +525,8 @@ uint32_t s_inst(uint32_t imm_s2, uint32_t rs2, uint32_t rs1, uint32_t funct3,
 
 // sb rs1, rs2, imm
 // M[ s[rs1] + sext(imm) ] = x[rs2][7:0]
-uint32_t sb(uint32_t rs1, uint32_t rs2, uint32_t imm) {
+uint32_t sb(uint32_t rs1, uint32_t rs2, uint32_t imm)
+{
   uint32_t imm_s2 = (imm) >> 5;
   uint32_t imm_s1 = (imm & 0b11111);
   return s_inst(imm_s2, rs2, rs1, FUNCT3_SB, imm_s1, OPCODE_SB);
@@ -505,7 +541,8 @@ uint32_t sb(uint32_t rs1, uint32_t rs2, uint32_t imm) {
 
 // sh rs1, rs2, imm
 // M[ s[rs1] + sext(imm) ] = x[rs2][15:0]
-uint32_t sh(uint32_t rs1, uint32_t rs2, uint32_t imm) {
+uint32_t sh(uint32_t rs1, uint32_t rs2, uint32_t imm)
+{
   uint32_t imm_s2 = (imm) >> 5;
   uint32_t imm_s1 = (imm & 0b11111);
   return s_inst(imm_s2, rs2, rs1, FUNCT3_SH, imm_s1, OPCODE_SH);
@@ -520,7 +557,8 @@ uint32_t sh(uint32_t rs1, uint32_t rs2, uint32_t imm) {
 
 // sw rs1, rs2, imm
 // M[ s[rs1] + sext(imm) ] = x[rs2]
-uint32_t sw(uint32_t rs1, uint32_t rs2, uint32_t imm) {
+uint32_t sw(uint32_t rs1, uint32_t rs2, uint32_t imm)
+{
   uint32_t imm_s2 = (imm) >> 5;
   uint32_t imm_s1 = (imm & 0b11111);
   return s_inst(imm_s2, rs2, rs1, FUNCT3_SW, imm_s1, OPCODE_SW);
@@ -531,11 +569,12 @@ uint32_t sw(uint32_t rs1, uint32_t rs2, uint32_t imm) {
  * ---------------------------------------------------------------------*/
 #define INST_J_MASK 0b00000000000000000000000001111111
 
-uint32_t j_inst(uint32_t imm, uint32_t rd, uint32_t opcode) {
-  uint32_t imm_j0 = ((imm & 0b100000000000000000000) >> 20);  // imm[20]
-  uint32_t imm_j1 = ((imm & 0b000000000011111111110) >> 1);   // imm[10:1]
-  uint32_t imm_j2 = ((imm & 0b000000000100000000000) >> 11);  // imm[11]
-  uint32_t imm_j3 = ((imm & 0b011111111000000000000) >> 12);  // imm[19:12]
+uint32_t j_inst(uint32_t imm, uint32_t rd, uint32_t opcode)
+{
+  uint32_t imm_j0 = ((imm & 0b100000000000000000000) >> 20); // imm[20]
+  uint32_t imm_j1 = ((imm & 0b000000000011111111110) >> 1);  // imm[10:1]
+  uint32_t imm_j2 = ((imm & 0b000000000100000000000) >> 11); // imm[11]
+  uint32_t imm_j3 = ((imm & 0b011111111000000000000) >> 12); // imm[19:12]
   uint32_t inst = (imm_j0 << 31);
   inst += (imm_j1 << 21);
   inst += (imm_j2 << 20);
@@ -561,11 +600,12 @@ uint32_t jal(uint32_t rd, uint32_t imm) { return j_inst(imm, rd, OPCODE_JAL); }
 #define INST_B_MASK 0b00000000000000000111000001111111
 
 uint32_t b_inst(uint32_t imm, uint32_t rs2, uint32_t rs1, uint32_t funct3,
-                uint32_t opcode) {
-  uint32_t imm_b0 = ((imm & 0b100000000000) >> 11);  // imm[12]
-  uint32_t imm_b1 = ((imm & 0b001111110000) >> 4);   // imm[10:5]
-  uint32_t imm_b2 = ((imm & 0b000000001110) >> 1);   // imm[4:1]
-  uint32_t imm_b3 = ((imm & 0b010000000000) >> 10);  // imm[11]
+                uint32_t opcode)
+{
+  uint32_t imm_b0 = ((imm & 0b100000000000) >> 11); // imm[12]
+  uint32_t imm_b1 = ((imm & 0b001111110000) >> 4);  // imm[10:5]
+  uint32_t imm_b2 = ((imm & 0b000000001110) >> 1);  // imm[4:1]
+  uint32_t imm_b3 = ((imm & 0b010000000000) >> 10); // imm[11]
   uint32_t inst = (imm_b0 << 31);
   inst += (imm_b1 << 25);
   inst += (rs2 << 20);
@@ -586,7 +626,8 @@ uint32_t b_inst(uint32_t imm, uint32_t rs2, uint32_t rs1, uint32_t funct3,
 
 // beq rs1, rs2, imm
 // if(x[rs1] == x[rs2]) then pc += sext(imm)
-uint32_t beq(uint32_t rs1, uint32_t rs2, uint32_t imm) {
+uint32_t beq(uint32_t rs1, uint32_t rs2, uint32_t imm)
+{
   return b_inst(imm, rs2, rs1, FUNCT3_BEQ, OPCODE_BEQ);
 }
 
@@ -599,7 +640,8 @@ uint32_t beq(uint32_t rs1, uint32_t rs2, uint32_t imm) {
 
 // bne rs1, rs2, imm
 // if(x[rs1] != x[rs2]) then pc += sext(imm)
-uint32_t bne(uint32_t rs1, uint32_t rs2, uint32_t imm) {
+uint32_t bne(uint32_t rs1, uint32_t rs2, uint32_t imm)
+{
   return b_inst(imm, rs2, rs1, FUNCT3_BNE, OPCODE_BNE);
 }
 
@@ -612,7 +654,8 @@ uint32_t bne(uint32_t rs1, uint32_t rs2, uint32_t imm) {
 
 // blt rs1, rs2, imm
 // if(x[rs1] <s x[rs2]) then pc += sext(imm)
-uint32_t blt(uint32_t rs1, uint32_t rs2, uint32_t imm) {
+uint32_t blt(uint32_t rs1, uint32_t rs2, uint32_t imm)
+{
   return b_inst(imm, rs2, rs1, FUNCT3_BLT, OPCODE_BLT);
 }
 
@@ -625,7 +668,8 @@ uint32_t blt(uint32_t rs1, uint32_t rs2, uint32_t imm) {
 
 // bge rs1, rs2, imm
 // if(x[rs1] >=s x[rs2]) then pc += sext(imm)
-uint32_t bge(uint32_t rs1, uint32_t rs2, uint32_t imm) {
+uint32_t bge(uint32_t rs1, uint32_t rs2, uint32_t imm)
+{
   return b_inst(imm, rs2, rs1, FUNCT3_BGE, OPCODE_BGE);
 }
 
@@ -638,7 +682,8 @@ uint32_t bge(uint32_t rs1, uint32_t rs2, uint32_t imm) {
 
 // bltu rs1, rs2, imm
 // if(x[rs1] <u x[rs2]) then pc += sext(imm)
-uint32_t bltu(uint32_t rs1, uint32_t rs2, uint32_t imm) {
+uint32_t bltu(uint32_t rs1, uint32_t rs2, uint32_t imm)
+{
   return b_inst(imm, rs2, rs1, FUNCT3_BLTU, OPCODE_BLTU);
 }
 
@@ -651,7 +696,8 @@ uint32_t bltu(uint32_t rs1, uint32_t rs2, uint32_t imm) {
 
 // bgeu rs1, rs2, imm
 // if(x[rs1] >=u x[rs2]) then pc += sext(imm)
-uint32_t bgeu(uint32_t rs1, uint32_t rs2, uint32_t imm) {
+uint32_t bgeu(uint32_t rs1, uint32_t rs2, uint32_t imm)
+{
   return b_inst(imm, rs2, rs1, FUNCT3_BGEU, OPCODE_BGEU);
 }
 
@@ -660,7 +706,8 @@ uint32_t bgeu(uint32_t rs1, uint32_t rs2, uint32_t imm) {
  * ---------------------------------------------------------------------*/
 #define INST_U_MASK 0b00000000000000000000000001111111
 
-uint32_t u_inst(uint32_t imm, uint32_t rd, uint32_t opcode) {
+uint32_t u_inst(uint32_t imm, uint32_t rd, uint32_t opcode)
+{
   uint32_t inst = (imm << 12);
   inst += (rd << 7);
   inst += opcode;
@@ -685,6 +732,7 @@ uint32_t lui(uint32_t rd, uint32_t imm) { return u_inst(imm, rd, OPCODE_LUI); }
 
 // auipc rd, imm
 // x[rd] = pc + sext(imm << 12)
-uint32_t auipc(uint32_t rd, uint32_t imm) {
+uint32_t auipc(uint32_t rd, uint32_t imm)
+{
   return u_inst(imm, rd, OPCODE_AUIPC);
 }
